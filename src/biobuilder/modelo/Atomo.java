817fbc96faf9ab040e0b2d084c91f644ec71d34d
EliasAtari <eliascanales110@gmail.com>
2026-08-25 23:11:23 -0500

    Create Atomo.java

---
+++ src/biobuilder/modelo/Atomo.java
@@ -0,0 +1,54 @@
+public class Atomo {
+  private int numeroAtomico;
+  private String simbolo;
+  private String nombre;
+
+private double masaAtomica;
+
+  private int protones;
+  private int neutrones;
+  private int electrones;
+
+private int enlacesMaximos;
+
+public Atomo(int
+ numeroAtomico, String simbolo, String nombre,
+             double
+    masaAtomica, int protones, int neutrones, int electrones, int enlacesMaximos) {
+    this.numeroAtomico = numeroAtomico;
+    this.simbolo = simbolo;
+    this.nombre = nombre;
+    this.masaAtomica = masaAtomica;
+    this.protones = protones;
+    this.neutrones = neutrones;
+    this.electrones = electrones;
+    this.enlacesMaximos = enlacesMaximos;
+}
+
+public int getNumeroAtomico() {
+  return numeroAtomico; 
+}
+public String getSimbolo() {
+  return simbolo;
+}
+public String getNombre() {
+  return nombre;
+}
+public double getMasaAtomica() {
+  return masaAtomica;
+}
+public int getProtones() {
+  return protones;
+}
+public int getNeutrones() {
+  return neutrones; 
+}
+public int getElectrones() {
+  return electrones;
+}
+public int getEnlacesMaximos() {
+  return enlacesMaximos;
+}
+}
+
+  
