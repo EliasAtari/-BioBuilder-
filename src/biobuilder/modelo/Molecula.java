81954034b3eeae96ec923c7bf07234421318fa95
EliasAtari <eliascanales110@gmail.com>
2026-08-26 21:14:09 -0500

    Update Molecula.java

--- src/biobuilder/modelo/Molecula.java
+++ src/biobuilder/modelo/Molecula.java
@@ -1,1 +1,28 @@
 package biobuilder.modelo;
+import java.util.ArrayList;
+  public class Molecula {
+    private String nombre;
+    private ArrayList<Atomo> atomos;
+    private ArrayList<Enlace> enlaces;
+
+  public Molecula (String nombre) {
+    this.nombre = nombre;
+    this.atomos = new ArrayList<>();
+    this.enlaces = new ArrayList<>();
+  }
+    public String getNombre() {
+      return nombre;
+    }
+    public ArrayList<Atomo> getAtomos() {
+      return atomos;
+    }
+    public ArrayList<Enlace> getEnlaces() {
+      return enlaces;
+    }
+    public void agregarAtomo(Atomo atomo) {
+      atomos.add(atomo);
+    }
+    public void agregarEnlace(Enlace enlace) {
+      enlaces.add(enlace);
+    }
+  }
