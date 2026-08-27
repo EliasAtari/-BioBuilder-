25dd9adf3b948aae0d74d2b865b55fb566bf8260
EliasAtari <eliascanales110@gmail.com>
2026-08-26 22:40:14 -0500

    Update Elemento.java

--- src/biobuilder/modelo/Elemento.java
+++ src/biobuilder/modelo/Elemento.java
@@ -2,19 +2,19 @@ package biobuilder.modelo;
 
 public class Elemento {
 
+    private String id;
     private int numeroAtomico;
     private String simbolo;
     private String nombre;
     private double masaAtomica;
-
     private int protones;
     private int neutrones;
     private int electrones;
-
     private int enlacesMaximos;
 
-    public Elemento(int numeroAtomico, String simbolo, String nombre,double masaAtomica, int protones, int neutrones, int electrones, int enlacesMaximos) {
+    public Elemento(String id, int numeroAtomico, String simbolo, String nombre,double masaAtomica, int protones, int neutrones, int electrones, int enlacesMaximos) {
 
+        this.id = id
         this.numeroAtomico = numeroAtomico;
         this.simbolo = simbolo;
         this.nombre = nombre;
@@ -24,7 +24,9 @@ public class Elemento {
         this.electrones = electrones;
         this.enlacesMaximos = enlacesMaximos;
     }
-
+    public String getId() {
+        return id;
+    }
     public int getNumeroAtomico() {
         return numeroAtomico;
     }
