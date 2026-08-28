68eb1bf10d51fc25b599024f8bfd5094ba48f6d2
EliasAtari <eliascanales110@gmail.com>
2026-08-27 22:41:06 -0500

    Update Plano2D.java

--- src/biobuilder/interfaz/Plano2D.java
+++ src/biobuilder/interfaz/Plano2D.java
@@ -4,6 +4,7 @@ public class Plano2D {
   private Molecula moleculaActual;
   private double ancho;
   private double alto;
+  private Atomo atomoSeleccionado;
 public Plano2D(double ancho, double alto) {
   this.ancho = ancho;
   this.alto = alto;
@@ -11,10 +12,29 @@ public Plano2D(double ancho, double alto) {
   public Molecula getMolecula() {
     return moleculaActual;
   }
+  public void setMoleculaActual(Molecula moleculaActual) {
+    this.moleculaActual = moleculaActual;
+  }
   public double getAncho() {
     return ancho;
   }
   public double getAlto() {
     return alto;
   }
+  public Atomo getAtomoSeleccionado() {
+    return atomoSeleccionado;
+  }
+  public void seleccionarAtomo(Atomo atomo) {
+    this.atomoSeleccionado = atomo;
+  }
+  public void moverAtomo(double nuevaX, double nuevaY) {
+    if (atomoSeleccionado !=null) {
+      atomoSeleccionado.setPosicionX(nuevaX);
+      atomoSeleccionado.setPosicionY(nuevaY);
+    }
+  }
+  public void soltarAtomo() {
+    atomoSeleccionado = null;
+  }
 }
+
