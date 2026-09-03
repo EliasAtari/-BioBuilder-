937fc4efc078df34ac20c59c7c919ad63d2c54f2
EliasAtari <eliascanales110@gmail.com>
2026-09-02 20:17:53 -0500

    Update and rename A to CarpetaBBM.java

---
+++ src/biobuilder/archivos/CarpetaBBM.java
@@ -0,0 +1,14 @@
+package biobuilder.archivos;
+
+public class CarpetaBBM {
+  private String nombre;
+  public CarpetaBBM(String nombre) {
+    this.nombre = nombre;
+  }
+  public String getNombre() {
+    return nombre;
+  }
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+}
