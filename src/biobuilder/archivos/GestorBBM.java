a61317f60ffc479918358af29f82cdc41aafd652
EliasAtari <eliascanales110@gmail.com>
2026-09-02 20:35:02 -0500

    Create GestorBBM.java

---
+++ src/biobuilder/archivos/GestorBBM.java
@@ -0,0 +1,11 @@
+package biobuilder.archivo;
+
+public class GestorBBM {
+  public void guardar(ArchivoBBM archivo) {
+  }
+  public ArchivoBBM abrir(String nombre) {
+    return null;
+  }
+  public void eliminar(String nombre) {
+  }
+}
