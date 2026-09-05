66b16c6cdebc135291c7877ec164d06f77b45986
EliasAtari <eliascanales110@gmail.com>
2026-09-04 19:14:24 -0500

    Create HerramientaEliminar.java

---
+++ src/biobuilder/constructor/HerramientaEliminar.java
@@ -0,0 +1,21 @@
+package biobuilder.constructor;
+
+public class HerramientaEliminar {
+  private boolean activa;
+
+public HerramientaEliminar() {
+  activa = false;
+}
+
+public boolean estaActiva() {
+  return activa;
+}
+
+public void activar() {
+  activa = true;
+}
+
+public void activar() {
+  activa = false;
+  }
+}
