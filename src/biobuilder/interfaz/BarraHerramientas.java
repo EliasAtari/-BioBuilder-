dc18a6224508531c503e0b3b55aae65c0e6e3d55
EliasAtari <eliascanales110@gmail.com>
2026-09-12 11:22:31 -0500

    Create BarraHerramientas.java

---
+++ src/biobuilder/interfaz/BarraHerramientas.java
@@ -0,0 +1,22 @@
+package biobuilder.interfaz;
+
+public class BarraHerramientas {
+  public static final String MOVER = "Mover";
+  public static final String ENLACE_SIMPLE = "Enlace Simple";
+  public static final String ENLACE_DOBLE = "Enlace Doble";
+  public static final String ENLACE_TRIPLE = "Enlace Triple";
+  public static final String ELIMINAR = "Eliminar";
+
+private String herramientaActual;
+
+public String
+  getHerramientaActual() {
+    return herramientaActual;
+  }
+
+public void
+  seleccionarHerramienta(String herramienta) {
+    herramientaActual = Herramienta;
+  }
+}
+    
